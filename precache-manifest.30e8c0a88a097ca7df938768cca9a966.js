2dd44b7dcc01a4a66630c557700e020829bc9f87
oschuang <oschuang1@gmail.com>
2020-03-30 19:37:12 -0400

    Updates

---
+++ precache-manifest.30e8c0a88a097ca7df938768cca9a966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65228e3d10cc2f8d557d9d92a8f7f62d",
+    "url": "/oschuang/tic-tac-toe_react/index.html"
+  },
+  {
+    "revision": "e2b991c006fbdd849d9c",
+    "url": "/oschuang/tic-tac-toe_react/static/css/main.5701c41a.chunk.css"
+  },
+  {
+    "revision": "4c00200b67824d2af30e",
+    "url": "/oschuang/tic-tac-toe_react/static/js/2.7e51485e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/oschuang/tic-tac-toe_react/static/js/2.7e51485e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b991c006fbdd849d9c",
+    "url": "/oschuang/tic-tac-toe_react/static/js/main.56a14933.chunk.js"
+  },
+  {
+    "revision": "93e7b5e423d69a61c768",
+    "url": "/oschuang/tic-tac-toe_react/static/js/runtime-main.08338e29.js"
+  }
+]);
